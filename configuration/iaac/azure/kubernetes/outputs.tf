d0cae43a9a5a52476794285d4418d467b12500e8
Thomas Henley <thomas.henley@maine.edu>
2022-04-18 16:11:39 -0400

    more sensitive output

--- configuration/iaac/azure/kubernetes/outputs.tf
+++ configuration/iaac/azure/kubernetes/outputs.tf
@@ -1,21 +1,26 @@
 output "client_key" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_key
+  sensitive = true
+  value     = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_key
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_certificate
+  sensitive = true
+  value     = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.cluster_ca_certificate
+  sensitive = true
+  value     = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.cluster_ca_certificate
 }
 
 output "cluster_username" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.username
+  sensitive = true
+  value     = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.username
 }
 
 output "cluster_password" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.password
+  sensitive = true
+  value     = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.password
 }
 
 output "kube_config" {
@@ -24,5 +29,6 @@ output "kube_config" {
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host
+  sensitive = true
+  value     = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host
 }
